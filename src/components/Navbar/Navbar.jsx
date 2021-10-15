10e466ddd6675a40563daf1baa7386cf598d8831
drjerry-sys <64374109+drjerry-sys@users.noreply.github.com>
2021-10-15 13:52:26 +0100

    just commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Menu, Typography, Avatar} from 'antd';
 import { Link } from 'react-router-dom';
 import icon from '../../assets/images/practiceLogo.png';
@@ -8,6 +8,25 @@ const { Title } = Typography;
 const { Item } = Menu;
 
 const Navbar = () => {
+
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [screenSize, setScreenSize] = useState(null);
+
+    useEffect(()=>{
+        const handleResize = () => setScreenSize(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        return ()=>window.removeEventListener('resize', handleResize);
+    }, [])
+
+    useEffect(()=> {
+        if (screenSize < 768) {
+            setActiveMenu(false);
+        } else {
+            setActiveMenu(true)
+        }
+    }, [screenSize])
+
     return ( 
         <div className="nav-container">
             <div className="logo-container">
@@ -15,21 +34,26 @@ const Navbar = () => {
                 <Title level={2} className="logo">
                     <Link to="/">Cryptoverse</Link>
                 </Title>
+                <Button className="menu-control-container" onClick={()=> setActiveMenu(!activeMenu)}>
+                    <MenuOutlined />
+                </Button>
             </div>
-            <Menu style={{backgroundColor: 'inherit'}}>
-                <Item icon={<HomeOutlined />} className="nav-containerMenuItem">
-                    <Link to="/" style={{color: 'white'}}>Home</Link>
-                </Item>
-                <Item icon={<FundOutlined />} className="nav-containerMenuItem">
-                    <Link to="/cryptocurrencies" style={{color: 'white'}}>Cryptocurrencies</Link>
-                </Item>
-                <Item icon={<MoneyCollectOutlined />} className="nav-containerMenuItem">
-                    <Link to="/exchanges" style={{color: 'white'}}>Exchanges</Link>
-                </Item>
-                <Item icon={<BulbOutlined />} className="nav-containerMenuItem">
-                    <Link to="/news" style={{color: 'white'}}>News</Link>
-                </Item>
-            </Menu>
+            {activeMenu && (
+                <Menu style={{backgroundColor: 'inherit'}}>
+                    <Item icon={<HomeOutlined />} className="nav-containerMenuItem">
+                        <Link to="/" style={{color: 'white'}}>Home</Link>
+                    </Item>
+                    <Item icon={<FundOutlined />} className="nav-containerMenuItem">
+                        <Link to="/cryptocurrencies" style={{color: 'white'}}>Cryptocurrencies</Link>
+                    </Item>
+                    <Item icon={<MoneyCollectOutlined />} className="nav-containerMenuItem">
+                        <Link to="/exchanges" style={{color: 'white'}}>Exchanges</Link>
+                    </Item>
+                    <Item icon={<BulbOutlined />} className="nav-containerMenuItem">
+                        <Link to="/news" style={{color: 'white'}}>News</Link>
+                    </Item>
+                </Menu>
+            )}
         </div>
      );
 }
--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -3,6 +3,7 @@ import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
 import moment from 'moment';
 import { useGetCryptoNewsQuery } from '../../services/cryptoNewsApi';
 import { useGetCryptosQuery } from '../../services/CryptoApi';
+import Loader from '../Loader';
 
 const { Text, Title } = Typography;
 const { Option } = Select;
@@ -13,7 +14,7 @@ const News = ({ simplified }) => {
     const [newsCategory, setNewsCategory] = useState('Cryptocurrency')
     const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
     const { data } = useGetCryptosQuery(100);
-    if (!cryptoNews?.value) return 'loading...';
+    if (!cryptoNews?.value) return <Loader />;;
 
     return ( 
         <Row gutter={[24, 24]}>
